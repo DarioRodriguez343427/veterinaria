98b1a45b06e34038e33316724999ff958e7d45d6
unknown <js316327@fi365.ort.edu.uy>
2026-02-08 22:14:33 -0300

    Corrección errores Login y Reservas

--- src/app.js
+++ src/app.js
@@ -1,10 +1,7 @@
-
 document.addEventListener('DOMContentLoaded', () => {
     const ruta = window.location.pathname;
 
-    console.log(ruta);
-
-    if(ruta === "/src/views/reservas.html"){
+    if (ruta === "/src/views/reservas.html") {
         document.querySelector("#fecha").setAttribute("min", fechaMinima());
         document.querySelector("#slcHora").innerHTML += cargarHorarios();
     }
@@ -14,41 +11,40 @@ document.addEventListener('DOMContentLoaded', () => {
      */
 });
 
-
-
-//Login
-
+//Login    
 const formLogin = document.querySelector("#login");
 
-formLogin.addEventListener("submit", function (evento) {
-    evento.preventDefault();
+if (formLogin) {
+    formLogin.addEventListener("submit", function (evento) {
+        evento.preventDefault();
 
-    const usuarioIngresado = document.querySelector("#usuario").value;
-    const contrasenaIngresada = document.querySelector("#contrasena").value;
+        const usuarioIngresado = document.querySelector("#usuario").value;
+        const contrasenaIngresada = document.querySelector("#contrasena").value;
 
-    login(usuarioIngresado, contrasenaIngresada);
+        login(usuarioIngresado, contrasenaIngresada);
     }
-);
+    );
+}
 
 // Reservas
 const formReservas = document.querySelector("#reservas");
 
+if (formReservas) {
+    formReservas.addEventListener("submit", function (evento) {
+        evento.preventDefault();
 
-formReservas.addEventListener("submit", function (evento) {
-    evento.preventDefault();
+        let nombreCliente = document.querySelector("#nombreCliente").value;
+        let celular = document.querySelector("#celular").value;
+        let correo = document.querySelector("#correo").value;
+        let nombreMascota = document.querySelector("#nombreMascota").value;
+        let opcionServicio = document.querySelector("#slcServicio").value;
+        let opcionProfesional = document.querySelector("#slcProfesional").value;
+        let fecha = document.querySelector("#fecha").value;
+        let opcionHora = document.querySelector("#slcHora").value;
 
-    let nombreCliente = document.querySelector("#nombreCliente").value;
-    let celular = document.querySelector("#celular").value;
-    let correo = document.querySelector("#correo").value;
-    let nombreMascota = document.querySelector("#nombreMascota").value;
-    let opcionServicio = document.querySelector("#slcServicio").value;
-    let opcionProfesional = document.querySelector("#slcProfesional").value;
-    let fecha = document.querySelector("#fecha").value;
-    let opcionHora = document.querySelector("#slcHora").value;
-
-    if (textoNulo(nombreCliente) || textoNulo(celular) || textoNulo(correo) || textoNulo(nombreMascota) || textoNulo(fecha) || opcionServicio == 0 || opcionProfesional == 0 || opcionHora == 0) {
-        document.querySelector("#pResultado").style.color = "red";
-        document.querySelector("#pResultado").innerHTML = "Todos los campos son obligatorios";
+        if (textoNulo(nombreCliente) || textoNulo(celular) || textoNulo(correo) || textoNulo(nombreMascota) || textoNulo(fecha) || opcionServicio == 0 || opcionProfesional == 0 || opcionHora == 0) {
+            document.querySelector("#pResultado").style.color = "red";
+            document.querySelector("#pResultado").innerHTML = "Todos los campos son obligatorios";
         } else {
             let errores = "";
             if (!tieneSoloLetras(nombreCliente)) {
@@ -68,12 +64,12 @@ formReservas.addEventListener("submit", function (evento) {
             if (errores !== "") {
                 document.querySelector("#pResultado").style.color = "red";
                 document.querySelector("#pResultado").innerHTML = errores;
-            } else {            
+            } else {
                 let reservas = JSON.parse(localStorage.getItem('reservas')) || [];
-                
+
                 const nuevaReserva = [nombreCliente, celular, correo, nombreMascota, opcionServicio, opcionProfesional, fecha, opcionHora];
                 reservas.push(nuevaReserva);
-                
+
                 localStorage.setItem('reservas', JSON.stringify(reservas));
 
                 document.querySelector("#pResultado").style.color = "black";
@@ -81,4 +77,5 @@ formReservas.addEventListener("submit", function (evento) {
             }
         }
     }
-);
+    );
+}
